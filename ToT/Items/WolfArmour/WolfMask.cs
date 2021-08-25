7e0e0c85f26c4250924a6c1d78ce13416109112e
ikee1 <isaachreid@gmail.com>
2021-08-25 12:33:43 +0100

    Just added the folders that weren't in, seems to be working now

---
+++ ToT/Items/WolfArmour/WolfMask.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ToT.Items
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class WolfMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Wolf Mask");
+            Tooltip.SetDefault("Harness your primitive instincts"
+                + "\nImmunity to 'Frostburn' debuff"
+                + "\n+1 max minion and 10% increased ranged damage");
+        }
+        public override void SetDefaults()
+        {
+            item.height = 13;
+            item.width = 13;
+            item.defense = 5;
+            item.rare = ItemRarityID.Blue;
+
+        }
+    }
+}
