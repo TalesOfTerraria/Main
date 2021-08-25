2d1c3d330e6df2a849a539982b2764aab0e6d35d
ikee1 <isaachreid@gmail.com>
2021-08-25 16:34:52 +0100

    Fixing stuff

---
+++ Items/WolfFang.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ToT.Items
+{
+    public class WolfFang : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Charged with the blood of an icy creature");
+        }
+        public override void SetDefaults()
+        {
+            item.height = 16;
+            item.width = 16;
+            item.rare = 2;
+            item.value = Item.sellPrice(gold: 2);
+            item.useStyle = 1;
+            item.noMelee = true;
+            item.maxStack = 999;
+        }
+    }
+}
---
+++ ToT.cs
@@ -0,0 +1,14 @@
+using Terraria.ModLoader;
+
+namespace ToT
+{
+    public class ToT : Mod
+    {
+        internal class Projectiles
+        {
+            internal class PrimordialArrow
+            {
+            }
+        }
+    }
+}
