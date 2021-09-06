9a2f3edba89f1386c744a4fedf9f56c667ed8d93
terra212x <62261361+terra212x@users.noreply.github.com>
2021-09-06 11:42:49 -0500

    Update PrimordialArrow.cs

--- Projectiles/PrimordialArrow.cs
+++ Projectiles/PrimordialArrow.cs
@@ -12,25 +12,26 @@ namespace ToT.Projectiles
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Primordial Arrow");
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
         }
 
         public override void SetDefaults()
         {
-            projectile.width = 8;
-            projectile.arrow = true;
-            projectile.height = 12;
-            projectile.aiStyle = 1;
-            projectile.friendly = true;
-            projectile.hostile = false;
-            projectile.ranged = true;
-            projectile.penetrate = 2;
-            projectile.timeLeft = 1000;
-            projectile.light = 0f;
-            projectile.ignoreWater = true;
-            projectile.tileCollide = true;
-            projectile.extraUpdates = 1;
-            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
-            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            Projectile.width = 8;
+            Projectile.arrow = true;
+            Projectile.height = 12;
+            Projectile.aiStyle = 1;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ranged = true;
+            Projectile.penetrate = 3;
+            Projectile.timeLeft = 1000;
+            Projectile.light = 0f;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 1;
+
             aiType = ProjectileID.WoodenArrowFriendly;
         }
       
@@ -38,17 +39,17 @@ namespace ToT.Projectiles
         {
             if (projectile.owner == Main.myPlayer && Main.rand.Next(4) == 0)
             {
-                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 116, projectile.oldVelocity.X * 0.1f, projectile.oldVelocity.Y * 0.1f);
+                Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, 116, Projectile.oldVelocity.X * 0.1f, Projectile.oldVelocity.Y * 0.1f);
             }
         }
 
 
         public override void Kill(int timeLeft)
         {
-            Main.PlaySound(0, (int)projectile.position.X, (int)projectile.position.Y, 1, 1f, 0f);
+            Main.PlaySound(0, (int)Projectile.position.X, (int)Projectile.position.Y, 1, 1f, 0f);
             for (int k = 0; k < 5; k++)
             {
-                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 0, projectile.oldVelocity.X * 0.1f, projectile.oldVelocity.Y * 0.1f);
+                Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, 0, Projectile.oldVelocity.X * 0.1f, Projectile.oldVelocity.Y * 0.1f);
             }
         }
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
