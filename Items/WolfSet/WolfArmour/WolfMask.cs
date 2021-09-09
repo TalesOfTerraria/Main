22b3e731e270f7b953fa0841d1e97cec3b29802c
ema116x <89397904+ema116x@users.noreply.github.com>
2021-09-09 23:03:12 +0200

    idk

---
+++ Items/WolfSet/WolfArmour/WolfMask.cs
@@ -0,0 +1,45 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ToT.Items.WolfSet.WolfArmour
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class WolfMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Wolf Mask");
+            Tooltip.SetDefault("Harness your primitive instincts"
+                + "\nImmunity to 'Frostburn' debuff"
+                + "\n+1 max minion and 10% increased ranged damage");
+        }
+        public override void SetDefaults()
+        {
+            item.height = 13;
+            item.width = 13;
+            item.defense = 5;
+            item.rare = ItemRarityID.Blue;
+
+        }
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ModContent.ItemType<WolfHide>() && legs.type == ModContent.ItemType<WolfHelmet>();
+        }
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = "Ranged and summon damage increased by 15%" +
+                "\n60% increased mining speed";
+            player.rangedDamage += 0.15f;
+            player.minionDamage += 0.15f;
+            player.pickSpeed += 0.6f;
+        }
+        public override void UpdateEquip(Player player)
+        {
+            player.buffImmune[BuffID.Frostburn] = true;
+            player.rangedDamage += 0.1f;
+            player.maxMinions += 1;
+        }
+    }
+}
---
+++ Items/WolfSet/WolfArmour/WolfModPlayer.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace ToT.Items.WolfSet.WolfArmour
+{
+    public class WolfModPlayer : ModPlayer
+    {
+        public bool FrostBurnRanged;
+        public bool LegsVelocity;
+        public override void ResetEffects()
+        {
+            FrostBurnRanged = false;
+            LegsVelocity = false;
+        }
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+        {
+            if (FrostBurnRanged == true)
+            {
+                if (((proj.ranged)) && FrostBurnRanged && !proj.noEnchantments)
+                {
+                    // If all those checks pass, we apply FrostBurn for some random duration.
+                    target.AddBuff(BuffID.Frostburn, 60 * Main.rand.Next(5, 15), false);
+                }
+            }
+        }
+    }
+    public class ArrowVelocity : GlobalItem
+    {
+        public override bool Shoot(Item item, Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (player.GetModPlayer<WolfModPlayer>().LegsVelocity == true)
+            {
+                
+
+            }
+            return base.Shoot(item, player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+        }
+    }
+}
