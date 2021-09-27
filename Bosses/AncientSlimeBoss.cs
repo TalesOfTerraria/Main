dad13c0762d2ec3e94e35bf732ee9864d7ba2056
ikee1 <isaachreid@gmail.com>
2021-09-27 22:06:30 +0100

    slime boss

---
+++ Bosses/AncientSlimeBoss.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace ToT.Bosses
+{
+    [AutoloadBossHead]
+    public class AncientSlime : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 2;
+            NPCID.Sets.DebuffImmunitySets.Add(Type, new NPCDebuffImmunityData
+            {
+                SpecificallyImmuneTo = new int[]
+                {
+                    BuffID.Frostburn,
+                    BuffID.Confused,
+                    BuffID.Frozen
+                }
+            });
+        public override void SetDefaults()
+        {
+            NPC.width = 70;
+            NPC.height = 40;
+        }
+    }
+    }
+}
