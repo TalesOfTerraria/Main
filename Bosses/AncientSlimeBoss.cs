21ae3eda46e2d47ef69c7cd435585b0ffb6c3d6c
ikee1 <isaachreid@gmail.com>
2021-09-30 21:03:48 +0100

    added/changed stuff

--- Bosses/AncientSlimeBoss.cs
+++ Bosses/AncientSlimeBoss.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.Utilities;
 
 namespace ToT.Bosses
 {
@@ -14,7 +17,7 @@ namespace ToT.Bosses
         public override void SetStaticDefaults()
         {
             Main.npcFrameCount[Type] = 2;
-            NPCID.Sets.DebuffImmunitySets.Add(Type, new NPCDebuffImmunityData
+            NPCDebuffImmunityData debuffData = new NPCDebuffImmunityData
             {
                 SpecificallyImmuneTo = new int[]
                 {
@@ -22,7 +25,8 @@ namespace ToT.Bosses
                     BuffID.Confused,
                     BuffID.Frozen
                 }
-            });
+            };
+            NPCID.Sets.DebuffImmunitySets.Add(Type, debuffData);
             NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers(0)
             {
                 CustomTexturePath = "ExampleMod/Assets/Textures/Bestiary/MinionBoss_Preview",
@@ -30,14 +34,51 @@ namespace ToT.Bosses
                 PortraitPositionYOverride = 0f,
             };
             NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+            NPCID.Sets.MPAllowedEnemies[Type] = true;
+            NPCID.Sets.BossBestiaryPriority.Add(Type);
         }
         public override void SetDefaults()
         {
             NPC.width = 70;
             NPC.height = 40;
             NPC.aiStyle = -1;
+            NPC.damage = 35;
+            NPC.lifeMax = 6000;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0f;
+            NPC.noGravity = false;
+            NPC.value = Item.buyPrice(gold: 5);
+            NPC.boss = true;
+            NPC.npcSlots = 10f;
+            int bossMusic = Main.rand.Next(1, 3);
+            if (bossMusic == 1)
+            {
+                Music = MusicID.Boss1;
+            }
+            else
+            {
+                Music = MusicID.Boss2;
+            }
+
+            NPC.BossBar = ModContent.GetInstance<AncientSlimeBossBar>();
+            BossBag = ModContent.ItemType<AncientSlimeBossBag>();
+        }
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            // Sets the description of this NPC that is listed in the bestiary
+            bestiaryEntry.Info.AddRange(new List<IBestiaryInfoElement> {
+                new MoonLordPortraitBackgroundProviderBestiaryInfoElement(), // Plain black background
+				new FlavorTextBestiaryInfoElement("Summoned from the depth of the ice itself, the Ancient Slime returns to destroy its puny challenger.")
+            });
+        }
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            //will do later, cba now
+        }
+        public override void AddRecipes()
+        {
 
         }
-    }
     }
 }
